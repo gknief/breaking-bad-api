66ad8349ced489f9095398c8a80cd4311d52a469
Grant Knief <gknief@mac.com>
2022-05-28 12:05:57 -0400

    displayed character cards on page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Header from './components/Header';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/Search';
 
 const App = () => {
   const [items, setItems] = useState ([]);
   const [isLoading, setIsLoading] = useState (true);
+  const [query, setQuery] = useState ('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
 
       setItems(result.data);
       setIsLoading(false);
@@ -23,6 +25,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
